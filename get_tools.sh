74a63c793f478aabd4b46a768e15604a8fb45e88
Francisco Ocegueda Jr. <36055163+focegueda1998@users.noreply.github.com>
2026-04-25 15:42:52 -0700

    Update get_tools.sh

--- get_tools.sh
+++ get_tools.sh
@@ -1,7 +1,7 @@
 apt-get update -y; \
 apt-get upgrade -y; \
 apt-get install -y zstd pciutils lshw; \
-curl -o /content/requirements.txt; \
+curl -o /content/requirements.txt https://raw.githubusercontent.com/focegueda1998/colab-tools/refs/heads/main/requirements.txt; \
 pip install --upgrade pip; \
 pip install uv; \
 uv pip install -r /content/requirements.txt; \
