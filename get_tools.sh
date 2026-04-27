288f5103f7595a6f1a1af6de40ff5c5f291c1ef1
Francisco Ocegueda Jr. <36055163+focegueda1998@users.noreply.github.com>
2026-04-27 16:30:20 -0700

    Modify pip install commands in get_tools.sh
    Updated uv pip install commands to include torch backend option.

--- get_tools.sh
+++ get_tools.sh
@@ -6,7 +6,6 @@ apt-get install -y zstd pciutils lshw; \
 curl -o /content/requirements.txt https://raw.githubusercontent.com/focegueda1998/colab-tools/refs/heads/main/requirements.txt; \
 pip install --upgrade pip; \
 pip install uv; \
-uv pip install -r /content/requirements.txt; \
-uv pip install --upgrade -r /content/requirements.txt; \
+uv pip install -r --torch-backend=auto /content/requirements.txt; \
 curl -fsSL https://ollama.com/install.sh | sh; \
 export DONE=1
