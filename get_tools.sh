65ddbce89e3fcd6d21a07fdce35788222b6381fe
Francisco Ocegueda Jr. <36055163+focegueda1998@users.noreply.github.com>
2026-04-30 18:04:41 -0700

    Fix paths for requirements.txt in get_tools.sh

--- get_tools.sh
+++ get_tools.sh
@@ -3,9 +3,9 @@
 apt-get update -y; \
 apt-get upgrade -y; \
 apt-get install -y zstd pciutils lshw; \
-curl -o /content/requirements.txt https://raw.githubusercontent.com/focegueda1998/colab-tools/refs/heads/main/requirements.txt; \
+curl -o ./requirements.txt https://raw.githubusercontent.com/focegueda1998/colab-tools/refs/heads/main/requirements.txt; \
 pip install --upgrade pip; \
 pip install uv; \
-uv pip install --torch-backend=auto -r /content/requirements.txt; \
+uv pip install --torch-backend=auto -r ./requirements.txt; \
 curl -fsSL https://ollama.com/install.sh | sh; \
 export DONE=1
